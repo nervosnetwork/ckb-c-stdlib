5149734fc87959319df6843febf267c7f2ba1d59
Xuejie Xiao <xxuejie@gmail.com>
2020-09-03 09:31:51 +0800

    fix: Review fixes

--- ckb_type_id.h
+++ ckb_type_id.h
@@ -12,6 +12,9 @@
 // from offset in current runnning script args, will be used as the actual type
 // ID.
 int ckb_validate_type_id(size_t offset);
+// Loading type ID from current script args, type_id must be at least 32 byte
+// long.
+int ckb_load_type_id(size_t offset, uint8_t* type_id);
 
 #ifndef CKB_TYPE_ID_DECLARATION_ONLY
 
@@ -21,7 +24,7 @@ int ckb_validate_type_id(size_t offset);
 #define DEBUG(s)
 #endif /* CKB_TYPE_ID_DEBUG */
 
-int _ckb_load_type_id(size_t offset, uint8_t* type_id) {
+int ckb_load_type_id(size_t offset, uint8_t* type_id) {
   // TODO: it is possible to simply load the script header, then only load the
   // 32 byte requested data based on offsets.
   uint8_t current_script[32768];
@@ -29,11 +32,11 @@ int _ckb_load_type_id(size_t offset, uint8_t* type_id) {
   int ret = ckb_load_script(current_script, &len, 0);
   if (ret != CKB_SUCCESS) {
     DEBUG("Cannot load current script!");
-    return CKB_USER_ERROR;
+    return CKB_INVALID_DATA;
   }
   if (len > 32768) {
     DEBUG("Current script is too large!");
-    return CKB_USER_ERROR;
+    return CKB_INVALID_DATA;
   }
 
   mol_seg_t script_seg;
@@ -41,14 +44,14 @@ int _ckb_load_type_id(size_t offset, uint8_t* type_id) {
   script_seg.size = len;
   if (MolReader_Script_verify(&script_seg, false) != MOL_OK) {
     DEBUG("Corrupted data!");
-    return CKB_USER_ERROR;
+    return CKB_INVALID_DATA;
   }
 
   mol_seg_t args_seg = MolReader_Script_get_args(&script_seg);
   mol_seg_t args_bytes_seg = MolReader_Bytes_raw_bytes(&args_seg);
   if (offset + 32 > args_bytes_seg.size) {
     DEBUG("Length of type id is incorrect!");
-    return CKB_USER_ERROR;
+    return CKB_INVALID_DATA;
   }
   memcpy(type_id, &args_bytes_seg.ptr[offset], 32);
   return CKB_SUCCESS;
@@ -73,7 +76,7 @@ int _ckb_locate_first_type_id_output_index(uint64_t* index) {
   }
   if (len != 32) {
     DEBUG("Invalid script hash length!");
-    return CKB_USER_ERROR;
+    return CKB_INVALID_DATA;
   }
 
   uint64_t i = 0;
@@ -88,7 +91,7 @@ int _ckb_locate_first_type_id_output_index(uint64_t* index) {
     }
     if (len != 32) {
       DEBUG("Invalid type hash length!");
-      return CKB_USER_ERROR;
+      return CKB_INVALID_DATA;
     }
     if (memcmp(buffer, current_script_hash, 32) == 0) {
       break;
@@ -103,8 +106,10 @@ int ckb_validate_type_id(size_t offset) {
   int has_second_type_id_input = _ckb_has_type_id_cell(1, 1);
   int has_second_type_id_output = _ckb_has_type_id_cell(1, 0);
   if (has_second_type_id_input || has_second_type_id_output) {
-    DEBUG("There can only be at most one input and one output type ID cell!");
-    return CKB_USER_ERROR;
+    DEBUG(
+        "There can only be at most one input and at most one output type ID "
+        "cell!");
+    return CKB_INVALID_DATA;
   }
 
   int has_first_type_id_input = _ckb_has_type_id_cell(0, 1);
@@ -112,7 +117,7 @@ int ckb_validate_type_id(size_t offset) {
     // We are creating a new type ID cell here. Additional checkings are needed
     // to ensure the type ID is legit.
     uint8_t current_type_id[32];
-    int ret = _ckb_load_type_id(offset, current_type_id);
+    int ret = ckb_load_type_id(offset, current_type_id);
     if (ret != CKB_SUCCESS) {
       return ret;
     }
@@ -134,12 +139,12 @@ int ckb_validate_type_id(size_t offset) {
     blake2b_state blake2b_ctx;
     blake2b_init(&blake2b_ctx, 32);
     blake2b_update(&blake2b_ctx, buffer, len);
-    blake2b_update(&blake2b_ctx, (uint8_t*)(&index), 8);
+    blake2b_update(&blake2b_ctx, (uint8_t*)(&index), sizeof(index));
     uint8_t expected_type_id[32];
     blake2b_final(&blake2b_ctx, expected_type_id, 32);
     if (memcmp(expected_type_id, current_type_id, 32) != 0) {
       DEBUG("Invalid type ID!");
-      return CKB_USER_ERROR;
+      return CKB_INVALID_DATA;
     }
   }
   return CKB_SUCCESS;
