23c85c7588b56f29f15dc7002b2e485d0e6df251
Xuejie Xiao <xxuejie@gmail.com>
2020-11-26 18:51:23 +0800

    feat: Split syscall APIs with core implementations (#19)
    This change defines a core set of CKB syscall APIs, it then separates
    utility functions that can be built on top of the core syscall
    APIs. As a result, the implementation of the core syscall APIs can be
    swapped, without affecting the utility functions one layer above. This
    enables simulator based solution, where we swap the core syscall APIs
    with dummy functions, so the same smart contracts can be built
    differently on multiple platforms: a RISC-V based version for
    execution on chain, and a x86 version for profiling and auditing reason.

---
+++ ckb_syscall_apis.h
@@ -0,0 +1,41 @@
+#ifndef CKB_C_STDLIB_CKB_SYSCALL_APIS_H_
+#define CKB_C_STDLIB_CKB_SYSCALL_APIS_H_
+
+/*
+ * Syscall related APIs that will be shared and used in all CKB
+ * smart contract environments
+ */
+
+#include <stddef.h>
+#include <stdint.h>
+
+int ckb_exit(int8_t code);
+int ckb_load_tx_hash(void* addr, uint64_t* len, size_t offset);
+int ckb_load_transaction(void* addr, uint64_t* len, size_t offset);
+int ckb_load_script_hash(void* addr, uint64_t* len, size_t offset);
+int ckb_load_script(void* addr, uint64_t* len, size_t offset);
+int ckb_debug(const char* s);
+
+int ckb_load_cell(void* addr, uint64_t* len, size_t offset, size_t index,
+                  size_t source);
+int ckb_load_input(void* addr, uint64_t* len, size_t offset, size_t index,
+                   size_t source);
+int ckb_load_header(void* addr, uint64_t* len, size_t offset, size_t index,
+                    size_t source);
+int ckb_load_witness(void* addr, uint64_t* len, size_t offset, size_t index,
+                     size_t source);
+int ckb_load_cell_by_field(void* addr, uint64_t* len, size_t offset,
+                           size_t index, size_t source, size_t field);
+int ckb_load_header_by_field(void* addr, uint64_t* len, size_t offset,
+                             size_t index, size_t source, size_t field);
+int ckb_load_input_by_field(void* addr, uint64_t* len, size_t offset,
+                            size_t index, size_t source, size_t field);
+int ckb_load_cell_data(void* addr, uint64_t* len, size_t offset, size_t index,
+                       size_t source);
+
+int ckb_dlopen2(const uint8_t* dep_cell_hash, uint8_t hash_type,
+                uint8_t* aligned_addr, size_t aligned_size, void** handle,
+                size_t* consumed_size);
+void* ckb_dlsym(void* handle, const char* symbol);
+
+#endif /* CKB_C_STDLIB_CKB_SYSCALL_APIS_H_ */
