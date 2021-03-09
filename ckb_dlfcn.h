d8be3cae114f187310d8bd7c50cfe13d356b96de
Xu Jiandong <xjd@cryptape.com>
2021-03-09 17:22:07 +0800

    draft changes for  TOB-NRD-001 to  TOB-NRD-011

--- ckb_dlfcn.h
+++ ckb_dlfcn.h
@@ -89,21 +89,67 @@ typedef struct {
 } Elf64_Rela;
 
 #define RISCV_PGSIZE 4096
-#define ROUNDUP(a, b) ((((a)-1) / (b) + 1) * (b))
-#define ROUNDDOWN(a, b) ((a) / (b) * (b))
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 
 #define ERROR_CONTEXT_FAILURE -21
 #define ERROR_INVALID_ELF -22
 #define ERROR_MEMORY_NOT_ENOUGH -23
+#define ERROR_OUT_OF_BOUND -24
+#define ERROR_INVALID_ARGS -25
+
 
 typedef struct {
   Elf64_Sym *dynsyms;
   const char *dynstr;
   size_t dynsym_size;
   uint8_t *base_addr;
+  size_t size;
 } CkbDlfcnContext;
+
+void padding_zero(uint8_t* buff, uint64_t size, uint64_t already_written) {
+  if (size > already_written) {
+    memset(buff + already_written, 0, size - already_written);
+  }
+}
+
+int check_in_range(const void* p, const CkbDlfcnContext* context) {
+  void* begin = (void*)(context->base_addr);
+  void* end = (void*)(context->base_addr + context->size);
+  if (begin <= p && p < end) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+// safe version of   #define ROUNDUP(a, b) ((((a)-1) / (b) + 1) * (b))
+int roundup(uint64_t a, uint64_t b, uint64_t* value) {
+  // when a == 0, (a-1)/b + 1 is zero, but we don't want to use this "behavior".
+  if (a == 0) {
+    *value = 0;
+    return 0;
+  }
+  uint64_t d = (a-1)/b;
+  return __builtin_umulll_overflow(d+1, b, value);
+}
+
+// here we have an assumption: the pointer uint8_t* has 64-bits.
+// it's true for CKB-VM
+uint8_t* addr_offset_checked(uint8_t* aligned_addr, uint64_t aligned_size, uint64_t offset) {
+  uint64_t target = 0;
+  // check in range
+  if (offset < aligned_size) {
+    // check overflow
+    if (__builtin_uaddll_overflow((uint64_t)aligned_addr, offset, &target)) {
+      return 0;
+    } else {
+      return (uint8_t*)target;
+    }
+  } else {
+    return 0;
+  }
+}
 
 int ckb_dlopen2(const uint8_t *dep_cell_hash, uint8_t hash_type,
                 uint8_t *aligned_addr, size_t aligned_size, void **handle,
@@ -111,12 +157,21 @@ int ckb_dlopen2(const uint8_t *dep_cell_hash, uint8_t hash_type,
   if (sizeof(CkbDlfcnContext) > RISCV_PGSIZE || aligned_size < RISCV_PGSIZE) {
     return ERROR_CONTEXT_FAILURE;
   }
+  if (dep_cell_hash == 0 || aligned_size == 0 || aligned_addr == 0 || handle == 0 || consumed_size == 0) {
+    return ERROR_INVALID_ARGS;
+  }
+  uint64_t _end = 0;
+  if (__builtin_uaddll_overflow((uint64_t)aligned_addr, aligned_size, &_end)) {
+    return ERROR_OUT_OF_BOUND;
+  }
 
   CkbDlfcnContext *context = (CkbDlfcnContext *)aligned_addr;
   memset(context, 0, sizeof(CkbDlfcnContext));
   aligned_addr += RISCV_PGSIZE;
   aligned_size -= RISCV_PGSIZE;
   context->base_addr = aligned_addr;
+  context->size = aligned_size;
+
 
   size_t index = SIZE_MAX;
   int ret = ckb_look_for_dep_with_hash2(dep_cell_hash, hash_type, &index);
@@ -158,14 +213,23 @@ int ckb_dlopen2(const uint8_t *dep_cell_hash, uint8_t hash_type,
       if ((ph->p_flags & PF_X) != 0) {
         uint64_t prepad = ph->p_vaddr % RISCV_PGSIZE;
         uint64_t vaddr = ph->p_vaddr - prepad;
-        uint64_t memsz = ROUNDUP(prepad + ph->p_memsz, RISCV_PGSIZE);
-        uint64_t size = 0;
+        uint64_t memsz = 0;
+        if (roundup(prepad + ph->p_memsz, RISCV_PGSIZE, &memsz)) {
+          return ERROR_INVALID_ELF;
+        }
+        unsigned long size = 0;
         if (__builtin_uaddl_overflow(vaddr, memsz, &size)) {
           return ERROR_INVALID_ELF;
         }
         if (size > aligned_size) {
           return ERROR_MEMORY_NOT_ENOUGH;
         }
+        // issue:
+        // 3.ckb_dlopen2 failsto zero-initialize executable segments
+        // it should be before call "_ckb_load_cell_code" since it will mark it as executable and
+        // can't write after that. (W ^ X)
+
+        padding_zero(aligned_addr + vaddr, memsz, ph->p_filesz);
         ret = _ckb_load_cell_code(aligned_addr + vaddr, memsz, ph->p_offset,
                                   ph->p_filesz, index, CKB_SOURCE_CELL_DEP);
         if (ret != CKB_SUCCESS) {
@@ -173,16 +237,31 @@ int ckb_dlopen2(const uint8_t *dep_cell_hash, uint8_t hash_type,
         }
         max_consumed_size = MAX(max_consumed_size, vaddr + memsz);
       } else {
-        uint64_t filesz = ph->p_filesz;
-        uint64_t size = 0;
+        uint64_t prepad = ph->p_vaddr % RISCV_PGSIZE;
+        uint64_t vaddr = ph->p_vaddr - prepad;
+
+        uint64_t filesz = 0;
+        if (roundup(prepad + ph->p_filesz, RISCV_PGSIZE, &filesz)) {
+          return ERROR_INVALID_ELF;
+        }
+        unsigned long size = 0;
         if (__builtin_uaddl_overflow(ph->p_vaddr, filesz, &size)) {
           return ERROR_INVALID_ELF;
         }
-        uint64_t consumed_end = ROUNDUP(size, RISCV_PGSIZE);
+        uint64_t consumed_end = 0;
+        if (roundup(size, RISCV_PGSIZE, &consumed_end)) {
+          return ERROR_INVALID_ELF;
+        }
         if (consumed_end > aligned_size) {
           return ERROR_MEMORY_NOT_ENOUGH;
         }
-        ret = ckb_load_cell_data(aligned_addr + ph->p_vaddr, &filesz,
+        // issue:
+        // 10. Potential integer overflowwhen loading non-executable segments
+        uint8_t* addr2 = addr_offset_checked(aligned_addr, aligned_size, vaddr);
+        if (addr2 == 0) {
+          return ERROR_INVALID_ARGS;
+        }
+        ret = ckb_load_cell_data(addr2, &filesz,
                                  ph->p_offset, index, CKB_SOURCE_CELL_DEP);
         if (ret != CKB_SUCCESS) {
           return ret;
@@ -206,6 +285,11 @@ int ckb_dlopen2(const uint8_t *dep_cell_hash, uint8_t hash_type,
     return ret;
   }
   if (len < sizeof(Elf64_Shdr) * header.e_shnum) {
+    return ERROR_INVALID_ELF;
+  }
+  // issue:
+  // 4. Potential out-of-boundsreadwhen reading the .shstrtab section header
+  if (header.e_shstrndx >= 32) {
     return ERROR_INVALID_ELF;
   }
   /*
@@ -243,16 +327,23 @@ int ckb_dlopen2(const uint8_t *dep_cell_hash, uint8_t hash_type,
         if (ret != CKB_SUCCESS) {
           return ret;
         }
-        if (len < load_size * sizeof(Elf64_Rela)) {
+        // issue
+        // 6.ckb_dlopen2 fails to check the returned buffer size when loading relocation entries
+        if (load_length < load_size * sizeof(Elf64_Rela)) {
           return ERROR_INVALID_ELF;
         }
         relocation_size -= load_size;
-        current_offset += len;
+        current_offset += load_length;
         for (size_t j = 0; j < load_size; j++) {
           Elf64_Rela *r = &relocations[j];
           if (r->r_info != R_RISCV_RELATIVE) {
             /* Only relative relocation is supported now, we might add more
              * later */
+            return ERROR_INVALID_ELF;
+          }
+          // issue:
+          // 7. Potential out-of-bounds writewhen processing relocations
+          if (r->r_offset >= aligned_size || r->r_addend >= aligned_size || r->r_addend < 0) {
             return ERROR_INVALID_ELF;
           }
           *((uint64_t *)(aligned_addr + r->r_offset)) =
@@ -264,12 +355,27 @@ int ckb_dlopen2(const uint8_t *dep_cell_hash, uint8_t hash_type,
       if (sh->sh_entsize != sizeof(Elf64_Sym)) {
         return ERROR_INVALID_ELF;
       }
+      // issue:
+      // 8. Validation missing when resolving the .dynstr and .dynsym sections
+      if (sh->sh_offset >= aligned_size) {
+        return ERROR_INVALID_ELF;
+      }
       context->dynsyms = (Elf64_Sym *)(aligned_addr + sh->sh_offset);
       context->dynsym_size = sh->sh_size / sh->sh_entsize;
     } else if (sh->sh_type == SHT_STRTAB) {
+      // issue:
+      // 5. Potential out-of-boundsreadwhen locating the .dynstrsection
+      char DYNSTR[] = ".dynstr";
       const char *current_str = shrtab + sh->sh_name;
-      if (strcmp(".dynstr", current_str) == 0) {
-        context->dynstr = (const char *)(aligned_addr + sh->sh_offset);
+      if (sh->sh_name < (4096-sizeof(DYNSTR))) {
+        if (strcmp(DYNSTR, current_str) == 0) {
+          // issue:
+          // 8. Validation missing when resolving the .dynstr and .dynsym sections
+          if (sh->sh_offset >= aligned_size) {
+            return ERROR_INVALID_ELF;
+          }
+          context->dynstr = (const char *)(aligned_addr + sh->sh_offset);
+        }
       }
     }
   }
@@ -287,8 +393,18 @@ void *ckb_dlsym(void *handle, const char *symbol) {
   for (uint64_t i = 0; i < context->dynsym_size; i++) {
     Elf64_Sym *sym = &context->dynsyms[i];
     const char *str = context->dynstr + sym->st_name;
+    // issue:
+    // 9. Possible out-of-bounds read in ckb_dlsym
+    if (!check_in_range(str, context)) {
+      return 0;
+    }
     if (strcmp(str, symbol) == 0) {
-      return (void *)(context->base_addr + sym->st_value);
+      void* p = (void *)(context->base_addr + sym->st_value);
+      if (!check_in_range(p, context)) {
+        return 0;
+      } else {
+        return p;
+      }
     }
   }
 
