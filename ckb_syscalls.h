4dfae3e3702f1ef2897d9eda721a20b6361144cb
mohanson <mohanson@outlook.com>
2023-07-06 17:57:06 +0800

    Rename peak_memory to current_memory

--- ckb_syscalls.h
+++ ckb_syscalls.h
@@ -402,8 +402,8 @@ int ckb_set_content(uint8_t* content, uint64_t* length) {
   return syscall(SYS_ckb_set_content, content, length, 0, 0, 0, 0);
 }
 
-int ckb_peak_memory() {
-  return syscall(SYS_ckb_peak_memory, 0, 0, 0, 0, 0, 0);
+int ckb_current_memory() {
+  return syscall(SYS_ckb_current_memory, 0, 0, 0, 0, 0, 0);
 }
 
 #endif /* CKB_STDLIB_NO_SYSCALL_IMPL */
